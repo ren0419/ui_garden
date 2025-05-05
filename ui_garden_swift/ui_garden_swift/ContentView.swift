45a25f5e89383c8a3c93408193b16c5ed137e862
ren0419 <renrenren0419@gmail.com>
2025-05-05 10:34:40 +0900

    swift project first commit

---
+++ ui_garden_swift/ui_garden_swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ui_garden_swift
+//
+//  Created by katahira ren on 2025/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Swift World!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ui_garden_swift/ui_garden_swift/ui_garden_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_garden_swiftApp.swift
+//  ui_garden_swift
+//
+//  Created by katahira ren on 2025/05/05.
+//
+
+import SwiftUI
+
+@main
+struct ui_garden_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
